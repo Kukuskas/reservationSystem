668451ef5f903b691afe52bd1a6678101cec630c
Filip Hladík <filip.hladik@plus4u.net>
2022-05-28 17:59:43 +0200

    findname

--- ReservationSystem/src/main/java/com/example/ReservationSystem/repository/LoginRepository.java
+++ ReservationSystem/src/main/java/com/example/ReservationSystem/repository/LoginRepository.java
@@ -4,8 +4,11 @@ import java.util.*;
 import com.example.ReservationSystem.DAO.MongoDao.User;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoginRepository extends MongoRepository<User,String> {
+    @Query("{},{_id:1}")
+    public List<User> findUserByName(String userName);
 }
