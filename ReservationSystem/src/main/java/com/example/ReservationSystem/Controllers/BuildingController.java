6e48bbf74fa20c169abe95781eeaf10c90b4ffe0
suchaiv1 <ivansuchanek8@gmail.com>
2022-05-28 15:34:33 +0200

    Adding building prefix

--- ReservationSystem/src/main/java/com/example/ReservationSystem/Controllers/BuildingController.java
+++ ReservationSystem/src/main/java/com/example/ReservationSystem/Controllers/BuildingController.java
@@ -8,11 +8,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/building")
 public class BuildingController {
     private final BuildingServices _buildingServices;
     @Autowired
