8cc67bf7dd2d5459d6277beacebd31dc1efeb65b
Filip Hladik <filip.hladik@unicorncollege.cz>
2022-05-28 11:56:50 +0200

    login dependenci

--- ReservationSystem/src/main/java/com/example/ReservationSystem/Controllers/LoginController.java
+++ ReservationSystem/src/main/java/com/example/ReservationSystem/Controllers/LoginController.java
@@ -1,4 +1,14 @@
 package com.example.ReservationSystem.Controllers;
 
+import com.example.ReservationSystem.BL.LoginServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class LoginController {
+    private final LoginServices _loginServices;
+	@Autowired
+	public  LoginController(LoginServices loginServices)
+    {
+        this._loginServices = loginServices;
+    }
 }
